af3b4bc2c05d51c8228ce9db7a27d27daf99c9c7
Vicenzo <79145837+vicenzofr@users.noreply.github.com>
2024-10-30 16:02:10 -0300

    sem cabeça

--- index/index.js
+++ index/index.js
@@ -1,14 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', function() {
-        const caixa = document.querySelector('.caixa');
-        if (window.scrollY > 50) { // Quando rolar mais de 50px
-            caixa.classList.add('scroll-active');
-        } else {
-            caixa.classList.remove('scroll-active');
+    const menuLinks = document.querySelectorAll('nav.links button a[href^="#"]');
+
+    function getDistanceFromTheTop(element) {
+        const id = element.getAttribute("href");
+        const targetElement = document.querySelector(id);
+
+        // Verifique se o elemento existe
+        if (targetElement) {
+            return targetElement.getBoundingClientRect().top + window.scrollY - 90; // Desloca para cima 90px
         }
+        return 0;
+    }
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const distanceFromTheTop = getDistanceFromTheTop(event.target);
+        
+        // Usar a rolagem suave nativa
+        window.scrollTo({
+            top: distanceFromTheTop,
+            behavior: "smooth" // Rolagem suave
+        });
+    }
+
+    menuLinks.forEach((link) => {
+        link.addEventListener("click", scrollToSection);
     });
-
-    
 });
-
-
